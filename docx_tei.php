3e5cf3ff0c263a750e8c76119a9f128c40f7036b
Frédéric Glorieux <frederic.glorieux@fictif.org>
2023-01-31 16:20:43 +0100

    ed1page

--- docx_tei.php
+++ docx_tei.php
@@ -12,7 +12,8 @@ Log::setLogger(new LoggerCli(LogLevel::DEBUG));
 if (!isset($argv[1])) {
     die("usage: php docx_tei.php examples/*.docx");
 }
-$docx_glob = $argv[1];
+// drop $argv[0], $argv[1…] should be file
+array_shift($argv);
 // destination directory for tei files
 $dst_dir = __DIR__ . '/out/';
 Filesys::mkdir($dst_dir);
@@ -23,18 +24,21 @@ $source->user_template(__DIR__ . '/galenus_tmpl.xml');
 // regex program to insert
 $source->user_pcre(__DIR__ . '/galenus_pcre.tsv');
 
-foreach (glob($docx_glob) as $docx_file) {
-    $src_name = pathinfo($docx_file, PATHINFO_FILENAME);
-    $dst_file = $dst_dir. $src_name .'.xml';
-    Log::info($docx_file . " > " . $dst_file);
-    $source->load($docx_file);
-    $source->tei();
-
-    // finalize with personal xslt
-    $xml = Xt::transformToXml(
-        __DIR__ . '/galenus.xsl',
-        $source->dom(),
-        ['filename' => $src_name]
-    );
-    file_put_contents($dst_file, $xml);
+// loop on arguments to get files of globs
+foreach ($argv as $glob) {
+    foreach (glob($glob) as $docx_file) {
+        $src_name = pathinfo($docx_file, PATHINFO_FILENAME);
+        $dst_file = $dst_dir. $src_name .'.xml';
+        Log::info($docx_file . " > " . $dst_file);
+        $source->load($docx_file);
+        $source->tei();
+    
+        // finalize with personal xslt
+        $xml = Xt::transformToXml(
+            __DIR__ . '/galenus.xsl',
+            $source->dom(),
+            ['filename' => $src_name]
+        );
+        file_put_contents($dst_file, $xml);
+    }
 }
